cdf5b5783a4d9313e66e55e294f402d7ec92c680
strongants <olejnikmateusz13@gmail.com>
2026-05-16 00:52:38 +0200

    Create stany.js

---
+++ stany.js
@@ -0,0 +1,15 @@
+const STANY = {
+
+"probowki-szklane": 3,
+"poidelko": 10,
+"peseta": 5,
+"mata-grzewcza": 2,
+"skalpel": 7,
+"pipety": 12,
+"miod": 6,
+"ziarna": 15,
+"karmidelko": 8,
+"strzykawka": 20,
+"butelka-tryskawka": 4
+
+};
