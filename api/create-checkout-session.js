0f30e1b25e44d1a57087c62d9da2fbd26f4c3181
strongants <olejnikmateusz13@gmail.com>
2026-05-22 17:39:09 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,1 +1,76 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+module.exports = async (req, res) => {
+
+if(req.method !== 'POST'){
+
+return res.status(405).json({
+error: 'Method not allowed'
+});
+
+}
+
+try{
+
+const { items } = req.body;
+
+const line_items = items.map(item => ({
+
+price_data: {
+
+currency: 'pln',
+
+product_data: {
+
+name: item.nazwa,
+
+images: [item.obraz]
+
+},
+
+unit_amount: Math.round(item.cena * 100)
+
+},
+
+quantity: item.ilosc
+
+}));
+
+const session = await stripe.checkout.sessions.create({
+
+payment_method_types: [
+'card',
+'blik'
+],
+
+line_items,
+
+mode: 'payment',
+
+success_url:
+'https://strongants.pl/dziekujemy.html',
+
+cancel_url:
+'https://strongants.pl/koszyk.html',
+
+});
+
+res.status(200).json({
+
+url: session.url
+
+});
+
+}catch(err){
+
+console.log(err);
+
+res.status(500).json({
+
+error: 'Stripe error'
+
+});
+
+}
+
+};
