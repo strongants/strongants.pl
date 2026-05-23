ba53480654df8bc904d21976ddd75f8c58e8141a
strongants <olejnikmateusz13@gmail.com>
2026-05-23 15:16:22 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,7 +1,6 @@
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-04-10',
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 });
 
 export default async function handler(req, res) {
