112c35d9ab2c0fc3aeb5b54edf372a1ec805f1c0
strongants <olejnikmateusz13@gmail.com>
2026-05-26 15:45:44 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -4,15 +4,25 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
 
+  console.log("START API");
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method not allowed'
+    });
+  }
+
   try {
 
-    if (req.method !== 'POST') {
-      return res.status(405).json({
-        error: 'Method not allowed'
-      });
-    }
+    console.log("BODY:", req.body);
 
     const { items } = req.body;
+
+    if (!items || !Array.isArray(items)) {
+      return res.status(400).json({
+        error: 'Brak items'
+      });
+    }
 
     const line_items = items.map(item => ({
       price_data: {
@@ -20,32 +30,36 @@ export default async function handler(req, res) {
         product_data: {
           name: item.nazwa || 'Produkt'
         },
-        unit_amount: Math.round(Number(item.cena) * 100),
+        unit_amount: Math.round((item.cena || 0) * 100)
       },
-      quantity: Number(item.ilosc) || 1,
+      quantity: item.ilosc || 1
     }));
+
+    console.log("LINE ITEMS:", line_items);
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items,
       mode: 'payment',
 
-      success_url: 'https://strongants.pl/dziekujemy.html',
-      cancel_url: 'https://strongants.pl/koszyk.html',
+      success_url: 'https://www.strongants.pl/dziekujemy.html',
+      cancel_url: 'https://www.strongants.pl/koszyk.html',
     });
 
-    res.status(200).json({
+    console.log("SESSION:", session.url);
+
+    return res.status(200).json({
       url: session.url
     });
 
   } catch (err) {
 
+    console.error("BŁĄD:");
     console.error(err);
 
-    res.status(500).json({
+    return res.status(500).json({
       error: err.message
     });
 
   }
-
 }
