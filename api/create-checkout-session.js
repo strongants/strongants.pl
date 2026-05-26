23fcd3c43fc4dd4d207956cac098c3ad9cd28cd5
strongants <olejnikmateusz13@gmail.com>
2026-05-26 17:54:34 +0200

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,35 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "API działa"
-  });
-}
+const Stripe = require("stripe");
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "pln",
+            product_data: {
+              name: "Strong Ants Produkt",
+            },
+            unit_amount: 6999,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "https://www.strongants.pl/dziekujemy.html",
+      cancel_url: "https://www.strongants.pl/koszyk.html",
+    });
+
+    res.status(200).json({
+      id: session.id,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
